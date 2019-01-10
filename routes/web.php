ac29012eaeabb53e591239ccdf746f54cb12b4e9
Codinget <codinget@163.com>
2019-01-10 14:49:13 +0800

    二维码测试

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+Route::get('home', 'HomeController@home')->name('home');
+
 Route::get('/alipay', 'PaymentController@alipay');
 Route::post('/alipay/notify', 'PaymentController@notify');
 Route::get('/alipay/return', 'PaymentController@return');
 
 Route::get('/wechat_pay', 'PaymentController@wechatPay');
 Route::post('/wechat/notify', 'PaymentController@wechatNotify');
+
+
